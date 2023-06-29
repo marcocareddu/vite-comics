555b9efb7dacc978ceb4ba9e183ea0ae5ceee99f
marcocareddu <saldumaccu@tuta.io>
2023-06-29 15:00:24 +0200

    VUE AppFooter - Create Markup

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,6 +6,76 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <footer>
+        <div class="uppper-foot">
+
+            <!-- Menu link -->
+            <div>
+                <h1>DC COMICS</h1>
+                <ul>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                </ul>
+                <h1>SHOP</h1>
+                <ul>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                </ul>
+            </div>
+            <div>
+                <h1>DC COMICS</h1>
+                <ul>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                </ul>
+            </div>
+            <div>
+                <h1>DC COMICS</h1>
+                <ul>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="lower-foot">
+
+            <!-- Sign-up -->
+            <div>
+                <button>SIGN-UP NOW!</button>
+            </div>
+
+            <!-- Social -->
+            <div>
+                <div>FOLLOW US</div>
+                <div>
+                    <ul>
+                        <li><a href="#"><img src="#" alt=""></a></li>
+                        <li><a href="#"><img src="#" alt=""></a></li>
+                        <li><a href="#"><img src="#" alt=""></a></li>
+                        <li><a href="#"><img src="#" alt=""></a></li>
+                        <li><a href="#"><img src="#" alt=""></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
 
 <style></style>
