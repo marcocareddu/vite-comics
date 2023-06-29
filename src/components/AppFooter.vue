cc26afd91998fe509845046ee465e3a02ef1582c
marcocareddu <saldumaccu@tuta.io>
2023-06-29 16:38:59 +0200

    VUE AppFooter - Added footerMenu array

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -23,7 +23,123 @@ export default {
                     text: "periscope logo",
                     picture: "src/assets/img/footer-periscope.png"
                 }
-            ]
+            ],
+            footerMenu: [
+                {
+                    title: "DC COMICS",
+                    link: [
+                        {
+                            name: "Characters",
+                            url: "#"
+                        },
+                        {
+                            name: "Comics",
+                            url: "#"
+                        },
+                        {
+                            name: "Movies",
+                            url: "#"
+                        },
+                        {
+                            name: "TV",
+                            url: "#"
+                        },
+                        {
+                            name: "Games",
+                            url: "#"
+                        },
+                        {
+                            name: "Videos",
+                            url: "#"
+                        },
+                        {
+                            name: "News",
+                            url: "#"
+                        }
+                    ],
+                    title: "SHOP",
+                    link: [
+                        {
+                            name: "Shop DC",
+                            url: "#"
+                        },
+                        {
+                            name: "Shop DC Collectibles",
+                            url: "#"
+                        },
+                    ],
+                    title: "DC",
+                    link: [
+                        {
+                            name: "Terms Of Use",
+                            url: "#"
+                        },
+                        {
+                            name: "Privacy policy (New)",
+                            url: "#"
+                        },
+                        {
+                            name: "Ad Choices",
+                            url: "#"
+                        },
+                        {
+                            name: "Advertising",
+                            url: "#"
+                        },
+                        {
+                            name: "Jobs",
+                            url: "#"
+                        },
+                        {
+                            name: "Subscritions",
+                            url: "#"
+                        },
+                        {
+                            name: "Talent Workshops",
+                            url: "#"
+                        },
+                        {
+                            name: "CPSC Certificates",
+                            url: "#"
+                        },
+                        {
+                            name: "Ratings",
+                            url: "#"
+                        },
+                        {
+                            name: "Shop Help",
+                            url: "#"
+                        },
+                        {
+                            name: "Contact Us",
+                            url: "#"
+                        },
+                    ],
+                    title: "SITES",
+                    link: [
+                        {
+                            name: "DC",
+                            url: "#"
+                        },
+                        {
+                            name: "MAD Magazine",
+                            url: "#"
+                        },
+                        {
+                            name: "DC Kids",
+                            url: "#"
+                        },
+                        {
+                            name: "DC Universe",
+                            url: "#"
+                        },
+                        {
+                            name: "DC Power Visa",
+                            url: "#"
+                        },
+                    ],
+                }
+            ],
         }
     }
 }
@@ -35,21 +151,25 @@ export default {
 
             <!-- Menu link -->
             <div>
-                <h1>DC COMICS</h1>
-                <ul>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                </ul>
-                <h1>SHOP</h1>
-                <ul>
-                    <li><a href="#">link</a></li>
-                    <li><a href="#">link</a></li>
-                </ul>
+                <div>
+                    <h1>DC COMICS</h1>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1>SHOP</h1>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </div>
             </div>
             <div>
                 <h1>DC COMICS</h1>
