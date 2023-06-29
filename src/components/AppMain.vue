b794d17b56818063e07b9e31245ad31a4a26cd3e
marcocareddu <saldumaccu@tuta.io>
2023-06-29 18:01:09 +0200

    VUE AppMain - Added Style

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,10 +7,27 @@ export default {
 </script>
 
 <template>
-    <!-- Main content -->
-    <section class="main-content">
-        <h1> Content goes here </h1>
-    </section>
+    <div class="main-content">
+
+        <!-- Main content -->
+        <section class="container">
+            <h1> Content goes here </h1>
+        </section>
+    </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/mixin' as *;
+@use '../assets/scss/partials/_customs' as *;
+
+.main-content {
+    height: 100px;
+    background-color: $black;
+}
+
+section {
+    height: 100%;
+    @include flex-between;
+    color: white;
+}
+</style>
