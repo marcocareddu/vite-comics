0100ae66c6292ec1c4630e3267a1d1edd2b7d3b3
marcocareddu <saldumaccu@tuta.io>
2023-06-29 14:34:49 +0200

    Added SASS & Created components

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,15 @@
-<script></script>
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+import BlueBand from './components/BlueBand.vue'
+
+export default {
+  name: 'DC Comics',
+  components: { AppHeader, AppMain, AppFooter, BlueBand }
+}
+
+</script>
 
 <template></template>
 
