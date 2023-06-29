d3dd1920f497068ad171320f7d421a6400195158
marcocareddu <saldumaccu@tuta.io>
2023-06-29 14:48:37 +0200

    VUE AppMain - Added Markup

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,6 +6,10 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <section class="main-content">
+        <h1> Content goes here </h1>
+    </section>
+</template>
 
 <style></style>
