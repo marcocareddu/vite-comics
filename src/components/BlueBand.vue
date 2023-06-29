18d98c83b1b77d3c6547e60a7c6afb9daca78a50
marcocareddu <saldumaccu@tuta.io>
2023-06-29 18:08:56 +0200

    SCSS Customs - Added blue

--- src/components/BlueBand.vue
+++ src/components/BlueBand.vue
@@ -38,11 +38,35 @@ export default {
 
 <template>
     <!-- Blue band Section -->
-    <div>
-        <ul v-for="item in blueMenu" :key="item.text">
-            <li><a href="#"><img :src="createImgUrl(item.picture)" alt="item.text">{{ item.text }}</a></li>
-        </ul>
+    <div class="blue-section">
+        <section class="container">
+            <ul v-for="item in blueMenu" :key="item.text">
+                <li><a href="#"><img :src="createImgUrl(item.picture)" alt="item.text">{{ item.text }}</a></li>
+            </ul>
+        </section>
     </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../assets/scss/partials/mixin' as *;
+@use '../assets/scss/partials/_customs' as *;
+
+.blue-section {
+    background-color: $blue;
+}
+
+section {
+    height: 100%;
+    @include flex-center;
+
+    ul li {
+        @include flex-center;
+    }
+
+    ;
+
+    img {
+        height: 50px;
+    }
+}
+</style>
