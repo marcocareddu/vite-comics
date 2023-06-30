90d0697269f3fb9cb1415054171036ef5ba7e808
marcocareddu <saldumaccu@tuta.io>
2023-06-30 15:16:30 +0200

    Vue Main - Added Jumbotron

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,7 +8,7 @@ export default {
 
 <template>
     <div class="main-content">
-
+        <div class="jumbotron"></div>
         <!-- Main content -->
         <section class="container">
             <h1> Content goes here </h1>
@@ -21,8 +21,15 @@ export default {
 @use '../assets/scss/partials/_customs' as *;
 
 .main-content {
-    height: 100px;
+    height: auto;
     background-color: $black;
+}
+
+.jumbotron {
+    height: 300px;
+    background-image: url(/src/assets/img/jumbotron.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
 }
 
 section {
