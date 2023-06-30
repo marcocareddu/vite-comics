0a573f91a0553589f0cd6de881f698a987111509
marcocareddu <saldumaccu@tuta.io>
2023-06-30 18:30:11 +0200

    VUE BlueBand - Added props - Removed data

--- src/components/BlueBand.vue
+++ src/components/BlueBand.vue
@@ -1,30 +1,7 @@
 <script>
 export default {
-    data() {
-        return {
-            blueMenu: [
-                {
-                    text: "DIGITAL COMICS",
-                    picture: "buy-comics-digital-comics.png"
-                },
-                {
-                    text: "DC MERCHANDISE",
-                    picture: "buy-comics-merchandise.png"
-                },
-                {
-                    text: "SUBSCRIPTION",
-                    picture: "buy-comics-subscriptions.png"
-                },
-                {
-                    text: "COMICS SHOP LOCATOR",
-                    picture: "buy-comics-shop-locator.png"
-                },
-                {
-                    text: "DC POWER EXTRA",
-                    picture: "buy-dc-power-visa.svg"
-                }
-            ]
-        }
+    props: {
+        blueMenu: Array,
     },
     methods: {
 
