d32da1f241969cf63ea6d329c977eff691e3b01c
marcocareddu <saldumaccu@tuta.io>
2023-06-30 16:17:44 +0200

    VUE - Added App Card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,20 @@
+<script></script>
+
+<template>
+    <div class="col">
+        <img src="https://imgs.search.brave.com/OU2E3_3vA7flyv-SJ3TcFW1d99KDlp5ZfmW9ylLLFDo/rs:fit:1012:1200:1/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vd3d3LnN1/cGVycG91dm9pci5j/b20vd3AtY29udGVu/dC91cGxvYWRzLzIw/MjAvMDcvQW1lcmlj/YW4tVmFtcGlyZS0x/OTc2LTAxLWNvdi1B/bGJ1cXVlcnF1ZS0y/MDIwLmpwZz9yZXNp/emU9MTAxMiUyQzE1/NTYmc3NsPTE"
+            alt="">
+        <p>Action comics</p>
+    </div>
+</template>
+
+<style scoped>
+img {
+    height: 100px;
+    width: 100px;
+}
+
+p {
+    text-transform: uppercase;
+}
+</style>
